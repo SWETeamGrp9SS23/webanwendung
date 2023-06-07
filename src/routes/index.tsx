30f699a05839659a4165a6b996d34d7c9e731752
vince <puvi1011@h-ka.de>
2023-06-07 15:51:36 +0200

    index angepasst Name

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,18 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import {BookSearch} from '../components/BookSearch';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { BookSearch } from "../components/BookSearch";
 
 export default component$(() => {
-  return (
-    <BookSearch/>
-  );
+  return <BookSearch />;
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Buecher suchen",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };
+
