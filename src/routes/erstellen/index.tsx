83efc0f2be281098a97c1228760bd85a846db584
Mischa Tettenborn <temi1011@h-ka.de>
2023-06-11 20:37:49 +0200

    .

--- src/routes/erstellen/index.tsx
+++ src/routes/erstellen/index.tsx
@@ -1,162 +1,245 @@
 import { component$, useSignal } from "@builder.io/qwik";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { gql } from "graphql-tag";
+import { createBooks } from "../../components/BookCreate";
 
-const client = new ApolloClient({
-  uri: "https://localhost:3000/graphql/",
-  cache: new InMemoryCache(),
-});
-
-export async function createBook(book: any) {
-  const CREATE_BOOK = gql`
-    mutation {
-      create(
-        input: {
-          isbn: ${book.isbn}
-          rating: ${book.rating}
-          art: ${book.art}
-          preis: ${book.preis}
-          rabatt: ${book.rabatt}
-          lieferbar: ${book.lieferbar}
-          datum: ${book.datum}
-          homepage: ${book.homepage}
-          schlagwoerter: ${book.schlagwoerter}
-          titel: {
-            titel: ${book.titel.titel}
-            untertitel: ${book.titel.untertitel}
-          }
-          //toDo Abbildungen fehlen
-        }
-      )
-    }
-  `;
-
-  const { data } = await client.mutate({
-    mutation: CREATE_BOOK,
-  });
-
-  return data;
+//toDo wie müssen wir createBooks einstellen?
+interface BookCreateProps {
+  bookService: BookService;
 }
 
-export const BookCreate = component$(() => {
-  const book = useSignal({
-    isbn: "",
-    rating: "",
-    art: "",
-    preis: "",
-    rabatt: "",
-    lieferbar: "",
-    datum: "",
-    homepage: "",
-    schlagwoerter: "",
-    titel: {
-      titel: "",
-      untertitel: "",
-    },
-    //toDo Abbildungen fehlen
-  });
+class BookService {
+  private createdBook: any | null = null;
+  setCreatedBook(book: any) {
+    this.createdBook = book;
+  }
+  getCreatedBook() {
+    return this.createdBook;
+  }
+}
+
+const bookService = new BookService();
+
+export default component$<BookCreateProps>(() => {
+  const setBook = useSignal("");
 
   return (
-    <>
-      <div class="book-create">
-        <h2>Neues Buch erstellen</h2>
-        <div class="form-group">
-          <label>ISBN:</label>
-          <input
-            type="text"
-            value={book.value.isbn}
-            onChange$={(e) => (book.value.isbn = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Rating:</label>
-          <input
-            type="number"
-            value={book.value.rating}
-            onChange$={(e) => (book.value.rating = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Art:</label>
-          <input
-            type="text"
-            value={book.value.art}
-            onChange$={(e) => (book.value.art = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Preis:</label>
-          <input
-            type="number"
-            value={book.value.preis}
-            onChange$={(e) => (book.value.preis = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Rabatt:</label>
-          <input
-            type="number"
-            value={book.value.rabatt}
-            onChange$={(e) => (book.value.rabatt = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Lieferbar:</label>
-          <input
-            type="checkbox"
-            //toDo hier anpassen lieferbar datentyp -> ändern
-            // checked={book.value.lieferbar}
-            // onChange$={(e) => (book.value.lieferbar = e.target.checked)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Datum:</label>
-          <input
-            type="date"
-            value={book.value.datum}
-            onChange$={(e) => (book.value.datum = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Homepage:</label>
-          <input
-            type="text"
-            value={book.value.homepage}
-            onChange$={(e) => (book.value.homepage = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Schlagwörter:</label>
-          <input
-            type="text"
-            value={book.value.schlagwoerter}
-            onChange$={(e) => (book.value.schlagwoerter = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Titel:</label>
-          <input
-            type="text"
-            value={book.value.titel.titel}
-            onChange$={(e) => (book.value.titel.titel = e.target.value)}
-          />
-        </div>
-        <div class="form-group">
-          <label>Untertitel:</label>
-          <input
-            type="text"
-            value={book.value.titel.untertitel}
-            onChange$={(e) => (book.value.titel.untertitel = e.target.value)}
-          />
-        </div>
+    <div class="container">
+      <h1>Buch erstellen</h1>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="ISBN"
+          aria-label="ISBN"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Bewertung"
+          aria-label="Bewertung"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Art"
+          aria-label="Art"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Preis"
+          aria-label="Preis"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Rabatt"
+          aria-label="Rabatt"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <select
+          class="form-control"
+          placeholder=""
+          aria-label=""
+          aria-describedby=""
+        >
+          <option value="" disabled selected>
+            Lieferbar
+          </option>
+          <option value={true}>Ja</option>
+          <option value={false}>Nein</option>
+        </select>
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="date"
+          class="form-control"
+          placeholder=""
+          aria-label=""
+          aria-describedby=""
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Homepage"
+          aria-label="Homepage"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Titel"
+          aria-label="Titel"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Untertitel"
+          aria-label="Untertitel"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Beschriftung"
+          aria-label="Beschriftung"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group mb-3">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Content Type"
+          aria-label="Content Type"
+          aria-describedby="button-addon2"
+        />
+      </div>
+      <div class="input-group-append">
         <button
-          onClick$={() => {
-            createBook(book.value);
+          class="btn btn-outline-secondary"
+          type="button"
+          id="button-addon2"
+          onClick$={async () => {
+            const isbnInput = document.querySelector(
+              'input[aria-label="ISBN"]',
+            );
+            const ratingInput = document.querySelector(
+              'input[aria-label="Bewertung"]',
+            );
+            const artInput = document.querySelector('input[aria-label="Art"]');
+            const preisInput = document.querySelector(
+              'input[aria-label="Preis"]',
+            );
+            const rabattInput = document.querySelector(
+              'input[aria-label="Rabatt"]',
+            );
+            const lieferbarInput = document.querySelector("select");
+            const datumInput = document.querySelector("input[type='date']");
+            const homepageInput = document.querySelector(
+              'input[aria-label="Homepage"]',
+            );
+            const titelInput = document.querySelector(
+              'input[aria-label="Titel"]',
+            );
+            const untertitelInput = document.querySelector(
+              'input[aria-label="Untertitel"]',
+            );
+            const beschriftungInput = document.querySelector(
+              "input[aria-label=Beschriftung]",
+            );
+            const contentTypeInput = document.querySelector(
+              "input[aria-label=Content Type]",
+            );
+
+            if (
+              isbnInput instanceof HTMLInputElement &&
+              ratingInput instanceof HTMLInputElement &&
+              artInput instanceof HTMLInputElement &&
+              preisInput instanceof HTMLInputElement &&
+              rabattInput instanceof HTMLInputElement &&
+              lieferbarInput instanceof HTMLSelectElement &&
+              datumInput instanceof HTMLInputElement &&
+              homepageInput instanceof HTMLInputElement &&
+              titelInput instanceof HTMLInputElement &&
+              untertitelInput instanceof HTMLInputElement &&
+              beschriftungInput instanceof HTMLInputElement &&
+              contentTypeInput instanceof HTMLInputElement
+            ) {
+              const isbn = isbnInput.value;
+              const rating = ratingInput.value;
+              const art = artInput.value;
+              const preis = preisInput.value;
+              const rabatt = rabattInput.value;
+              const lieferbar = lieferbarInput.value;
+              const datum = datumInput.value;
+              const homepage = homepageInput.value;
+              const titel = titelInput.value;
+              const untertitel = untertitelInput.value;
+              const beschriftung = beschriftungInput.value;
+              const contentType = contentTypeInput.value;
+
+              console.log("Erstelle Buch mit ISBN: " + isbn);
+
+              const book = await createBook({
+                input: {
+                  isbn,
+                  rating,
+                  art,
+                  preis,
+                  rabatt,
+                  lieferbar,
+                  datum,
+                  homepage,
+                  titel: {
+                    titel,
+                    untertitel,
+                  },
+                  abbildungen: [
+                    {
+                      beschriftung,
+                      contentType,
+                    },
+                  ],
+                },
+              });
+              console.log("Erstelltes Buch: " + book);
+
+              // eslint-disable-next-line qwik/valid-lexical-scope
+              bookService.setCreatedBook(book);
+              console.log(
+                "Buch im BookService:",
+                JSON.stringify(bookService.getCreatedBook(), null, 2),
+              );
+              setBook.value = bookService.getCreatedBook().buch;
+            }
           }}
         >
           Buch erstellen
         </button>
       </div>
-    </>
+    </div>
   );
 });
