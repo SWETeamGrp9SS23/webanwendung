e4436ff010a2bfad5bdfb7c68199a3f83e0f6704
Student <yannik.wuest@web.de>
2023-06-15 12:17:35 +0200

    Erstellen von Büchern. (Fehlendes mitsenden JWT)

--- src/components/BookCreate.tsx
+++ src/components/BookCreate.tsx
@@ -1,39 +1,38 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { gql } from "graphql-tag";
+import { gql } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://localhost:3000/graphql/",
   cache: new InMemoryCache(),
 });
 
-export async function createBooks(input: any) {
+export async function createBooks(isbn: String, homepage: String, titel: String, 
+                                untertitel: String, beschriftung: String, contentType: String) {
   console.log("Ich bin in createBooks!");
   const CREATE_data = gql`
-    mutation CreateBook($input: CreateBookInput!) {
-      createBook(input: $input) {
-        isbn
-        rating
-        art
-        preis
-        rabatt
-        lieferbar
-        datum
-        homepage
-        titel {
-          titel
-          untertitel
+    mutation Create($isbn: String!, $homepage: String!, $titel: String!, $untertitel: String!,
+                    $beschriftung: String!, $contentType: String!) {
+      create( input: {
+          isbn: $isbn, 
+          homepage: $homepage,
+          titel: {
+            titel: $titel,
+            untertitel: $untertitel
+          },
+          abbildungen: [{
+            beschriftung: $beschriftung,
+            contentType: $contentType
+          }]
         }
-        abbildungen {
-          beschriftung
-          contentType
-        }
-      }
+      )
     }
   `;
+
   console.log(CREATE_data);
   const { data } = await client.mutate({
     mutation: CREATE_data,
-    variables: { input },
+    variables: {  isbn, homepage, titel, 
+                  untertitel, beschriftung, contentType },
   });
   console.log(data);
 
